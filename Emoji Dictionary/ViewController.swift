c506d6b0fed8b77a873b2267ad48cdac37b2667e
Alexander Abushady <aabushady214@gmail.com>
2019-07-11 10:40:06 -0400

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Alexander Abushady on 7/11/19.
+//  Copyright © 2019 Alexander Abushady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
